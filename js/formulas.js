26deb98f901929622db6a5862515d9f67d18c3a1
John Redden <johnleightonredden@gmail.com>
2020-11-24 20:45:57 -0500

    removed redundant value definitions and simplified use of pi

--- js/formulas.js
+++ js/formulas.js
@@ -1,31 +1,3 @@
-//Values
-
-// let num1 = Number(prompt("Enter a number:"));
-// let num2 = Number(prompt("Enter a second number:"));
-//let side = Number(prompt("How long is a side of the square?"));
-// let length = Number(prompt("What is the length?"));
-// let width = Number(prompt("What is the width?"));
-// let base = Number(prompt("What is the length of the base?"));
-// let height = Number(prompt("What is the height?"));
-//let radius = Number(prompt("What is the radius?"));
-// let side1 = Number(prompt("How long is the first side?"));
-// let side2 = Number(prompt("How long is the second side?"));
-// let side3 = Number(prompt("How long is the third side?"));
-//let diameter = Number(prompt("What is the diameter?"));
-
-let num1 = 4;
-let num2 = 5;
-let side = 6;
-let length = 8;
-let width = 12;
-let base = 9;
-let height = 5;
-let radius = 10;
-let π = Math.PI;
-let side1 = 2;
-let side2 = 7;
-let side3 = 3;
-let diameter = 15;
 
 // Basic math formulas
 
@@ -64,11 +36,11 @@ function areaTriangle(base, height){
 }
 
 function Circle(radius){
-  return π * Math.pow(radius, 2);
+  return Math.PI * Math.pow(radius, 2);
 }
 
 function Sphere(radius){
-  return 4 * π * Math.pow(radius, 2);
+  return 4 * Math.PI * Math.pow(radius, 2);
 }
 
 // Surface Area formulas
@@ -77,7 +49,7 @@ function surfaceAreaCube(side){
 }
 
 function surfaceAreaCylinder(radius, height){
-  return 2 * π * radius * height;
+  return 2 * Math.PI * radius * height;
 }
 
 // Perimeter formulas
@@ -94,7 +66,7 @@ function perimeterTriangle(side1, side2, side3){
 }
 
 function perimeterCircle(diameter){
-  return π * diameter;
+  return Math.PI * diameter;
 }
 
 // Volume formulas
@@ -107,5 +79,5 @@ function volumeRectangular(length, width, height){
 }
 
 function volumeCylinder(radius, height){
-  return π * Math.pow(radius, 2) * height;
+  return Math.PI * Math.pow(radius, 2) * height;
 }
