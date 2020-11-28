6b7814014bee052bc6f4ad3d5c0fef112a1ddb59
CWilliams75 <coreywllms9@gmail.com>
2020-11-28 14:40:00 -0500

    Perimeter formulas

--- js/formulas.js
+++ js/formulas.js
@@ -41,45 +41,45 @@ function areaRectangle(length, width){
 
 
 function areaParallelogram(base, height){
-  return -1;
+  return base * height;
 }
 
 function areaTriangle(base, height){
-  return -1;
+  return (base * height) / 2;
 }
 
 function Circle(radius){
-  return -1;
+  return Math.pow(radius, 2) * Math.PI;
 }
 
 function Sphere(radius){
-  return -1;
+  return 4 * Math.PI * Math.pow(radius, 2);
 }
 
 // Surface Area formulas
 function surfaceAreaCube(side){
-  return -1;
+  return 6 * Math.pow(side, 2);
 }
 
 function surfaceAreaCylinder(radius, height){
-  return -1;
+  return 2 * Math.PI * radius * height;
 }
 
 // Perimeter formulas
 function perimeterSquare(side){
-  return -1;
+  return 4 * side;
 }
 
 function perimeterRectangle(length, height){
-  return -1;
+  return (2 * length) + (2 * height);
 }
 
 function perimeterTriangle(side1, side2, side3){
-  return -1;
+  return side1 + side2 + side3;
 }
 
 function perimeterCircle(diameter){
-  return -1;
+  return Math.PI * diameter;
 }
 
 // Volume formulas
