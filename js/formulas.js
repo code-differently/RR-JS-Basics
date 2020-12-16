0c73656a501ee31f7ab1121b699123c329675c25
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-16 18:51:41 -0500

    function perimeterRectangle(length, width) - changed width from height

--- js/formulas.js
+++ js/formulas.js
@@ -1,61 +1,61 @@
-// Basic math formulaas
+// Basic math formulas
 function addition(num1, num2){
-  return -1;
+  return num1 + num2;
 }
 
 function subtraction(num1, num2){
-  return -1;
+  return num1 - num2;
 }
 
 function multiplication(num1, num2){
-  return -1;
+  return num1 * num2;
 }
 
 function division(num1, num2){
-  return -1;
+  return num1 / num2;
 }
 
-// Area formulaas
+// Area formulas
 function areaSquare(side){
-  return -1;
+  return Math.pow(side, 2);
 }
 
 function areaRectangle(length, width){
-  return -1;
+  return length * width;
 }
 
 function areaParallelogram(base, height){
-  return -1;
+  return base * height;
 }
 
 function areaTriangle(base, height){
-  return -1;
+  return (1 / 2) * (base * height);
 }
 
 function Circle(radius){
-  return -1;
+  return Math.PI(Math.pow(radius, 2));
 }
 
 function Sphere(radius){
-  return -1;
+  return 4 * Math.PI * (Math.pow(radius, 3) / 3);
 }
 
 // Surface Area formulas
 function surfaceAreaCube(side){
-  return -1;
+  return 6 * Math.pow(side, 2);
 }
 
 function surfaceAreaCylinder(radius, height){
-  return -1;
+  return 2 * Math.PI * radius * height;
 }
 
 // Perimeter formulas
 function perimeterSquare(side){
-  return -1;
+  return 4 * side;
 }
 
-function perimeterRectangle(length, height){
-  return -1;
+function perimeterRectangle(length, width){
+  return (2 * length) + (2 * width);
 }
 
 function perimeterTriangle(side1, side2, side3){
