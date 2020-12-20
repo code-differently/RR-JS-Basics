b1b475c3ca80d506cee4dfefe71fdbfc0b746e09
AntsmithCode <hey@antsmith.dev>
2020-12-19 19:23:05 -0500

    Completed the functions to return the proper values.

--- js/formulas.js
+++ js/formulas.js
@@ -1,80 +1,80 @@
-// Basic math formulaas
-function addition(num1, num2){
-  return -1;
+// Basic math formulas
+function addition(num1, num2) {
+  return num1 + num2;
 }
 
-function subtraction(num1, num2){
-  return -1;
+function subtraction(num1, num2) {
+  return num1 - num2;
 }
 
-function multiplication(num1, num2){
-  return -1;
+function multiplication(num1, num2) {
+  return num1 * num2;
 }
 
-function division(num1, num2){
-  return -1;
+function division(num1, num2) {
+  return num1 / num2;
 }
 
-// Area formulaas
-function areaSquare(side){
-  return -1;
+// Area formulas
+function areaSquare(side) {
+  return Math.pow(side, 2);
 }
 
-function areaRectangle(length, width){
-  return -1;
+function areaRectangle(length, width) {
+  return length * width;
 }
 
-function areaParallelogram(base, height){
-  return -1;
+function areaParallelogram(base, height) {
+  return base * height;
 }
 
-function areaTriangle(base, height){
-  return -1;
+function areaTriangle(base, height) {
+  return (base * height) / 2;
 }
 
-function Circle(radius){
-  return -1;
+function Circle(radius) {
+  return Math.pow(radius, 2) * Math.PI; // Math.PI uses the number PI & Math.Pow(X,Y) X to the power of Y
 }
 
-function Sphere(radius){
-  return -1;
+function Sphere(radius) {
+  return 4 * Math.PI * Math.pow(radius, 2);
 }
 
 // Surface Area formulas
-function surfaceAreaCube(side){
-  return -1;
+function surfaceAreaCube(side) {
+  return 6 * Math.pow(side, 2);
 }
 
-function surfaceAreaCylinder(radius, height){
-  return -1;
+function surfaceAreaCylinder(radius, height) {
+  return 2 * Math.PI * radius * height;
 }
 
 // Perimeter formulas
-function perimeterSquare(side){
-  return -1;
+function perimeterSquare(side) {
+  return 4 * side;
 }
 
-function perimeterRectangle(length, height){
-  return -1;
+function perimeterRectangle(length, height) {
+  return 2 * length + 2 * height;
 }
 
-function perimeterTriangle(side1, side2, side3){
-  return -1;
+function perimeterTriangle(side1, side2, side3) {
+  return side1 + side2 + side3;
 }
 
-function perimeterCircle(diameter){
-  return -1;
+function perimeterCircle(diameter) {
+  return Math.PI * diameter;
 }
 
 // Volume formulas
-function volumeCube(side){
-  return -1;
+function volumeCube(side) {
+  return Math.pow(side, 3);
 }
 
-function volumeRectangular(length, width, height){
-  return -1;
+function volumeRectangular(length, width, height) {
+  return length * width * height;
 }
 
-function volumeCylinder(radius, height){
-  return -1;
+function volumeCylinder(radius, height) {
+  return Math.PI * Math.pow(radius, 2) * height;
 }
